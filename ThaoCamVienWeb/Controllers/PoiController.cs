f22ffb1e7e51483afaab280acc15660538f1d0d4
Nguyễn Tấn Dũng <156091609+Wolf0170@users.noreply.github.com>
2026-03-12 13:56:04 +0700

    Thay đổi một số cấu trúc

---
+++ ThaoCamVienWeb/Controllers/PoiController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThaoCamVienWeb.Models;
+
+public class PoiController : Controller
+{
+    private readonly WebContext _context;
+
+    public PoiController(WebContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var data = _context.Pois.ToList();
+        return View(data);
+    }
+}
